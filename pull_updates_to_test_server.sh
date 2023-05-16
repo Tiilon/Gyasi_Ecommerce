d0842ac25e9c40b11e89b652de0851d691bdf663
Tiilon <tiilon42@gmail.com>
2023-05-16 20:29:16 +0000

    Using jenkins to deploy to test server

--- pull_updates_to_test_server.sh
+++ pull_updates_to_test_server.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-ssh -i ~/.ssh/id_rsa 18.205.194.28 <<EOF
-  sudo -i
-  cd /var/www/html/Gyasi_Ecommerce
-  git pull origin main
-  . venv/bin/activate
-  pip install -r requirements.txt
-  python3 manage.py makemigrations
-  python3 manage.py migrate  --run-syncdb
-  sudo systemctl reload apache2.service
-  exit
-EOF
+#ssh -i ~/.ssh/id_rsa 18.205.194.28 <<EOF
+ssh -i ~/.ssh/id_rsa 18.205.194.28
+sudo -i
+cd /var/www/html/Gyasi_Ecommerce
+git pull origin main
+. venv/bin/activate
+pip install -r requirements.txt
+python3 manage.py makemigrations
+python3 manage.py migrate  --run-syncdb
+sudo systemctl reload apache2.service
+exit
+#EOF
